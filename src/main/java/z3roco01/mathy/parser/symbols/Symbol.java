de49eddb3436025db00448cf12758764712dad8a
zerocool <46464404+z3roco01@users.noreply.github.com>
2025-04-25 18:14:26 -0500

    add map to symbol enum

--- src/main/java/z3roco01/mathy/parser/symbols/Symbol.java
+++ src/main/java/z3roco01/mathy/parser/symbols/Symbol.java
@@ -1,19 +1,35 @@
 package z3roco01.mathy.parser.symbols;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 public enum Symbol {
-    ADD('+', "ADD"),
-    SUB('-', "SUB"),
-    MUL('*', "MUL"),
-    DIV('/', "DIV"),
-    EXPO('^', "EXPO"),
-    LEFT_PAREN('(', "LEFT_PAREN"),
-    RIGHT_PAREN(')', "RIGHT_PAREN");
+    ADD('+'),
+    SUB('-'),
+    MUL('*'),
+    DIV('/'),
+    EXPO('^'),
+    LEFT_PAREN('('),
+    RIGHT_PAREN(')'),
+    NUM('\0'); // does not have a symbol, since it will be parsed dif
 
-
-    final char symbol;
-    final String name; // used mostly for debu
-    private Symbol(char Symbol, String name) {
-        this.symbol = Symbol;
-        this.name = name;
+    /**
+     * the character that corisponds to this symbol
+     * will be \0 if there isnt one
+     */
+    final Character character;
+    private Symbol(Character Symbol) {
+        this.character = Symbol;
     }
+
+    private static Map<Character, Symbol> createMap() {
+        HashMap<Character, Symbol> map = new HashMap<>();
+        for(Symbol symbol : Symbol.values())
+            map.put(symbol.character, symbol);
+
+        return map;
+    }
+
+    static Map<Character, Symbol> char2SymbolMap = Collections.unmodifiableMap(createMap());
 }
